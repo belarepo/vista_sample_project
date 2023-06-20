fb749f8bf177a8f44c9704899b365116a1ffcb72
Mahdi Asgari Gholami <mahdi.asgari.gholami@proton.me>
2023-06-20 17:08:43 +0330

    set ejs as view engine

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ import 'dotenv/config';
 // create express app
 import express from 'express';
 const app = express();
+
+// set __dirname
+const __dirname = path.dirname(new URL('views', import.meta.url).pathname);
+
+// ejs view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
