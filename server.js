a77b785a84b5ab640eebe9f171887cedd1668f72
Mahdi Asgari Gholami <mahdi.asgari.gholami@proton.me>
2023-06-20 17:10:55 +0330

    add serve static files

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ import 'dotenv/config';
 import express from 'express';
 const app = express();
 
-// set __dirname
+// declare __dirname
+import path from 'path';
+import { URL } from 'url';
 const __dirname = path.dirname(new URL('views', import.meta.url).pathname);
 
 // ejs view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// serve static files
+app.use(express.static(path.join(__dirname, 'public'), { index: false }));
